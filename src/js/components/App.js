05d57d16ab6eb57f585619f78ea4ebf005b714eb
nick weinberg <nickweinberg90@gmail.com>
2015-06-03 04:00:09 -0700

    testing add another component

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,13 +1,15 @@
 var React = require('React');
 var Router = require('react-router');
 var RouteHandler = Router.RouteHandler;
+var Parent = require('./Parent');
+
 
 var App = React.createClass({
   render: function(){
     return (
       <div className="main-container">
         <div className="inner-container">
-          <div> Watch out!!! </div>
+          <Parent />
           <RouteHandler />
           <div> FOOTER </div>
         </div>
---
+++ src/js/components/Parent.js
@@ -0,0 +1,15 @@
+var React = require('React');
+
+var Parent = React.createClass({
+
+    render: function() {
+        return (
+            <div>
+              Parent component.
+            </div>
+        );
+    }
+
+});
+
+module.exports = Parent;
